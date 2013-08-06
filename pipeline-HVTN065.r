d653b038d676e4232e4ff7a992a3b7f8f49497e1
John Ramey <johnramey@gmail.com>
2013-08-06 12:50:19 -0700

    Extract MFI for each cytokine for both CD4 and CD8 cells.

--- pipeline-HVTN065.r
+++ pipeline-HVTN065.r
@@ -35,5 +35,27 @@ save_gs(gs_HVTN065, path = "/loc/no-backup/ramey/HVTN/065/gating-set", overwrite
 popstats_HVTN065 <- getPopStats(gs_HVTN065)
 counts_HVTN065 <- getPopStats(gs_HVTN065, statistic = "count")
 
-save(counts_HVTN065, popstats_HVTN065, pData_HVTN065, file = "data/HVTN065-results.RData")
+# Extracts MFI for each sample
+cytokine_markers <- data.frame(channel = c("Alexa 680-A", "PE Cy7-A", "PE Green laser-A"),
+                               marker = c("TNFa", "IFNg", "IL2"), stringsAsFactors = FALSE)
+
+fs_cd4 <- getData(gs_HVTN065, "cd4")[, cytokine_markers$channel]
+fs_cd8 <- getData(gs_HVTN065, "cd8")[, cytokine_markers$channel]
+
+
+mfi_cd4 <- sapply(sampleNames(fs_cd4), function(fcs_file) {
+  each_col(fs_cd4[[fcs_file]], median)
+})
+mfi_cd4 <- data.table(melt(mfi_cd4))
+setnames(mfi_cd4, colnames(mfi_cd4), c("Cytokine", "Sample", "MFI"))
+
+mfi_cd8 <- sapply(sampleNames(fs_cd8), function(fcs_file) {
+  each_col(fs_cd8[[fcs_file]], median)
+})
+mfi_cd8 <- data.table(melt(mfi_cd8))
+setnames(mfi_cd8, colnames(mfi_cd8), c("Cytokine", "Sample", "MFI"))
+
+
+save(counts_HVTN065, popstats_HVTN065, pData_HVTN065, mfi_cd4, mfi_cd8,
+     file = "data/HVTN065-results.RData")
 
